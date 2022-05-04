de66f79903f9ba3bab9b10a1ca65eb90c47ecc91
Brady Dukart <bdukart@bushelpowered.com>
2022-05-04 14:13:21 -0500

    init

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+# Day 1: Band Name Generator
+
+# print("Band Name Generator")
+# city = input("What city were you born in?\n")
+# pet_name = input("What was your first pet's name?\n")
+
+# print("Band Name: " + pet_name + " " + city)
